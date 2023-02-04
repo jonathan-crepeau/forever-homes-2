24303801aea25cbc80a6154eede3128e348e49dd
jonathan-crepeau <jon.crepeau@gmail.com>
2023-02-03 16:11:51 -0800

    successful search update for location & type + reformated styling for cards & drop down menus

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,19 +1,21 @@
 // console.log('search.js reporting for duty..');
 
 // SECTION - Variables (bindings):
-// let tokenTimeStamp = 0;
 let petData;
-// const main = document.querySelector('main');
+const paramsObj = {};
+
 const resultsContainer = document.getElementById('result-cont');
 const logoutButton = document.getElementById('logout-btn');
-const searchButton = document.getElementById('search-title');
+const searchTitle = document.getElementById('search-title');
+const searchDropdown = document.getElementById('search-dropdown');
+const searchButton = document.getElementById('search-btn');
 
 
 // SECTION - Event Listeners
 logoutButton.addEventListener('click', handleLogoutClick);
 resultsContainer.addEventListener('click', expandCardClick);
-searchButton.addEventListener('click', searchBtnClick);
-
+searchTitle.addEventListener('click', handleSearchDrop);
+searchButton.addEventListener('click', handleSearchUpdate);
 
 
 // SECTION - Function Declarations
@@ -41,10 +43,22 @@ function expandCardClick(event) {
     }
 }
 
-function searchBtnClick(event) {
-    event.preventDefault();
-    document.getElementById('search-drop-down').classList.toggle('closed');
+function handleSearchDrop() {
+    searchDropdown.classList.toggle('closed');
 }
+
+function handleSearchUpdate(event) {
+    let count = 0;
+    document.querySelectorAll('.query-param').forEach((input) => {
+        paramsObj[count] = {key: input.id, value: input.value}
+        count += 1;
+        input.value = '';
+    })
+    getPetData(paramsObj);
+    searchDropdown.classList.add('closed');
+}
+
+
 
 function verifyUser() {
     fetch('http://localhost:3000/api/v1/verify', {
