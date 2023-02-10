a02393ba2a39d2011b919ac49399f717933e1c33
jonathan-crepeau <jon.crepeau@gmail.com>
2023-02-09 21:34:26 -0800

    update scripts/login.js to have single dual-form

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,52 +1,51 @@
 console.log('login.js reporting for duty..');
 
-/*
-1. Get form data
-2. Validate form data values
-3. Submit request if valid
-4. Redirect to /profile if successful
-*/
+// SECTION - Global Variables (Bindings)
+// const form = document.getElementById("login-form");
+
+// SECTION - Event Listeners
+// form.addEventListener('submit', handleSubmitClick);
+
+// SECTION - Function Declarations
+// function handleSubmitClick(event) {
+//     event.preventDefault();
+//     let formIsValid = true;
+//     const userData = {};
 
 
-const form = document.getElementById("login-form");
+//     const formInputs = [...form.elements];
+//     formInputs.forEach((input) => {
+//         input.classList.remove('input-error');
+//         if (input.type !== 'submit' && input.value === "") {
+//             formIsValid = false;
+//             input.classList.add('input-error');
+//         } else if (input.type === "password" && input.value.length < 4) {
+//             formIsValid = false;
+//             input.classList.add('input-error');
+//         }
 
-const handleSubmitClick = (event) => {
-    event.preventDefault();
-    let formIsValid = true;
-    const userData = {};
+//         if (formIsValid) {
+//             userData[input.name] = input.value;
+//         }
+//     });
 
+//     if (formIsValid) {
+//         fetch('http://localhost:3000/api/v1/login', {
+//             method: "POST",
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 'credentials': 'include'
+//             },
+//             body: JSON.stringify(userData)
+//         })
+//             .then((response) => response.json())
+//             .then((data) => {
+//                 console.log(data);
+//                 window.location = '/search';
+//             });
+//     }
+// }
 
-    const formInputs = [...form.elements];
-    formInputs.forEach((input) => {
-        input.classList.remove('input-error');
-        if (input.type !== 'submit' && input.value === "") {
-            formIsValid = false;
-            input.classList.add('input-error');
-        } else if (input.type === "password" && input.value.length < 4) {
-            formIsValid = false;
-            input.classList.add('input-error');
-        }
-
-        if (formIsValid) {
-            userData[input.name] = input.value;
-        }
-    });
-
-    if (formIsValid) {
-        fetch('http://localhost:3000/api/v1/login', {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-                'credentials': 'include'
-            },
-            body: JSON.stringify(userData)
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                window.location = '/search';
-            });
-    }
-}
-
-form.addEventListener('submit', handleSubmitClick);
+$('a').click(function(){
+    $('form').animate({height: 'toggle', opacity: 'toggle'}, 'slow');
+});
