29cad675c82240a70537687a8612f137623b1f2f
jonathan-crepeau <jon.crepeau@gmail.com>
2023-02-26 14:44:32 -0800

    make <main> flexible and update styling for cards

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,3 +7,86 @@ btn.addEventListener('click', (event) => {
     event.preventDefault();
     navList.classList.toggle('closed');
 })
+
+// function helloEcho() {
+//     console.log('Hello, hello, hello from app.js... app.js ...');
+// }
+
+function createCard(input) {
+
+    const card = `
+        <div class="card">
+            <div class="card__header">
+                <div class="card__pet-photo">
+                    <img src="https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80" alt=""> 
+                </div>
+                <div class="card__pet-titles"></div>
+                <div class="card__favorite-icon"></div>
+            </div>
+            <div class="card__hero-photo"></div>
+            <div class="card__text"></div> 
+        </div>
+    `;
+    return card;
+    
+
+    // let photoSource;
+    // if (!input.primary_photo_cropped) {
+    //     photoSource = "https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80";
+    // } else {
+    //     photoSource = input.primary_photo_cropped['small'];
+    // }
+
+    // let string = '';
+    // for (let a = 0; a < input.tags.length - 1; a++) {
+    //     string = string + `#${input.tags[a]} `;
+    // }
+
+    // let attributesString = '';
+    // let attributes = input.attributes;
+    // for (let x in attributes) {
+    //     if (attributes[x] !== null) {
+    //         attributesString = attributesString + `[${x}: ${attributes[x]}] `;
+    //     }
+    // }
+
+    // const vettedAttributes = petAttributes(input);
+
+    // const card = `
+    //     <div class="card closed">
+    //         <div class="card__photo">
+    //             <img src=${photoSource} />
+    //         </div>
+    //         <div class="card__info closed">
+    //             <div class="card__header">
+    //                 <h1 class="card__title">${inputArray[i].name} (${inputArray[i].species})</h1>
+    //                 <p class="card__btn">&#9660</p>
+    //                 <p class="card__fav">&#9734</p>
+    //             </div>
+    //             <div class="card__writtenDesc">
+    //                 <p>${inputArray[i].gender} • ${inputArray[i].age} • ${inputArray[i].size}</p>
+    //                 <p>${inputArray[i].contact.address.city}, ${inputArray[i].contact.address.state}</p>
+    //                 <p class="tags">${string}<p>
+    //                 <br>
+    //                 <p>Breed: ${inputArray[i].breeds.primary}</p>
+    //                 <p class="attr">${vettedAttributes}</p>
+                    
+    //             </div>
+    //         </div>
+    //     </div>
+    // `
+}
+
+
+function petAttributes(inputObj) {
+    const attributes = inputObj.attributes;
+    let string = '';
+    for (let attr in attributes) {
+        if (attributes[attr] !== null) {
+            string = string + `${attr}: ${attributes[attr]} <br>`;
+        }
+    }
+    string = string.replace(/true/g, '<span>&#9989;</span>');
+    string = string.replace(/false/g, '<span>&#10060;</span>')
+    return string;
+}
--- public/scripts/search.js
+++ public/scripts/search.js
@@ -132,7 +132,7 @@ function getPetData(bodyObj) {
             .then((response) => response.json())
             // .then((data) => console.log(data))
             .then((data) => petData = data.animals)
-            .then(petData => createCards(petData))
+            .then(petData => displayCards(petData))
             .catch((error) => console.log(error));
     } else {
         fetch('http://localhost:3000/petfinder/v1/petData', {
@@ -140,62 +140,18 @@ function getPetData(bodyObj) {
         })
             .then((response) => response.json())
             .then((data) => petData = data.animals)
-            .then(petData => createCards(petData))
+            .then(petData => displayCards(petData))
             .catch((error) => console.log(error))
     }
 }
 
-function createCards(inputArray) {
+
+function displayCards(inputArray) {
     resultsContainer.innerHTML = '';
     for (let i = 0; i < inputArray.length - 1; i++) {
-        let photoSource;
-        if (!inputArray[i].primary_photo_cropped) {
-            photoSource = "https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80";
-        } else {
-            photoSource = inputArray[i].primary_photo_cropped['small'];
-        }
-
-        let string = '';
-        for (let a = 0; a < inputArray[i].tags.length - 1; a++) {
-            string = string + `#${inputArray[i].tags[a]} `;
-        }
-
-        let attributesString = '';
-        let attributes = inputArray[i].attributes;
-        for (let x in attributes) {
-            if (attributes[x] !== null) {
-                attributesString = attributesString + `[${x}: ${attributes[x]}] `;
-            }
-        }
-
-        const vettedAttributes = petAttributes(inputArray[i]);
-
-        const card = `
-            <div class="card closed">
-                <div class="card__photo">
-                    <img src=${photoSource} />
-                </div>
-                <div class="card__info closed">
-                    <div class="card__header">
-                        <h1 class="card__title">${inputArray[i].name} (${inputArray[i].species})</h1>
-                        <p class="card__btn">&#9660</p>
-                        <p class="card__fav">&#9734</p>
-                    </div>
-                    <div class="card__writtenDesc">
-                        <p>${inputArray[i].gender} • ${inputArray[i].age} • ${inputArray[i].size}</p>
-                        <p>${inputArray[i].contact.address.city}, ${inputArray[i].contact.address.state}</p>
-                        <p class="tags">${string}<p>
-                        <br>
-                        <p>Breed: ${inputArray[i].breeds.primary}</p>
-                        <p class="attr">${vettedAttributes}</p>
-                        
-                    </div>
-                </div>
-            </div>
-        `
-        
-        
-    resultsContainer.insertAdjacentHTML('beforeend', card)    
+        // console.log(inputArray[i])
+        let card = createCard(inputArray[i]);
+        resultsContainer.insertAdjacentHTML('beforeend', card)    
     }
 }
 
@@ -211,19 +167,6 @@ function createQueryString(input) {
         .then((response) => response.json())
         .then((data) => console.log(data))
         .catch((error) => console.log(error));
-}
-
-function petAttributes(inputObj) {
-    const attributes = inputObj.attributes;
-    let string = '';
-    for (let attr in attributes) {
-        if (attributes[attr] !== null) {
-            string = string + `${attr}: ${attributes[attr]} <br>`;
-        }
-    }
-    string = string.replace(/true/g, '<span>&#9989;</span>');
-    string = string.replace(/false/g, '<span>&#10060;</span>')
-    return string;
 }
 
 
